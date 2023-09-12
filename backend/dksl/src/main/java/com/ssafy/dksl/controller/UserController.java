94700e018079363b39d8ff5a11aa5c242d2b5906
You Young <dbdud1999@naver.com>
2023-09-12 14:09:25 +0900

    :sparkles: (유영) 회원가입 구현 완료

--- backend/dksl/src/main/java/com/ssafy/dksl/controller/UserController.java
+++ backend/dksl/src/main/java/com/ssafy/dksl/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.ssafy.dksl.controller;
 
 import com.ssafy.dksl.model.dto.UserDto;
-import com.ssafy.dksl.model.service.UserService;
+import com.ssafy.dksl.model.service.UserServiceImpl;
+import com.ssafy.dksl.util.exception.RegisterException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,15 +13,19 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 public class UserController {
 
-    private UserService userService;
+    private final UserServiceImpl userService;
 
     @Autowired
-    private UserController(UserService userService) {
+    UserController(UserServiceImpl userService) {
         this.userService = userService;
     }
 
     @PostMapping("register")
     private ResponseEntity<?> registerUser(@RequestBody UserDto userDto) {
-        return ResponseEntity.ok(userService.registerUser(userDto));
+        try {
+            return ResponseEntity.ok(userService.registerUser(userDto));
+        } catch (RegisterException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);  // 에러 츌력
+        }
     }
 }
--- backend/dksl/src/main/java/com/ssafy/dksl/model/service/UserService.java
+++ backend/dksl/src/main/java/com/ssafy/dksl/model/service/UserService.java
@@ -1,9 +1,10 @@
 package com.ssafy.dksl.model.service;
 
 import com.ssafy.dksl.model.dto.UserDto;
+import com.ssafy.dksl.util.exception.RegisterException;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
-    public boolean registerUser(UserDto userDto);
+    public boolean registerUser(UserDto userDto) throws RegisterException;
 }
