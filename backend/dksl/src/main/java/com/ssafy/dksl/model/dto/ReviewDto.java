c033262404ac34be5edd974ee9fc97bdaf30d2c4
김태훈 <zaqxsw1397@naver.com>
2023-09-18 20:12:08 +0900

    :sparkles: (태훈) 투기장 댓글 dto 생성

---
+++ backend/dksl/src/main/java/com/ssafy/dksl/model/dto/ReviewDto.java
@@ -0,0 +1,27 @@
+package com.ssafy.dksl.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ssafy.dksl.model.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.hibernate.annotations.Comment;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class ReviewDto {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private LocalDateTime createdAt;
+
+    private UserDto userDto;
+
+    private String matchId;
+
+    private String content;
+}
