baaf33d9391a33f08527f2586a976aa001391cf4
김태훈 <zaqxsw1397@naver.com>
2023-09-20 17:22:57 +0900

    :sparkles: (태훈) 댓글 수정과 삭제 관련 controller 메소드 생성

--- backend/dksl/src/main/java/com/ssafy/dksl/controller/ReviewController.java
+++ backend/dksl/src/main/java/com/ssafy/dksl/controller/ReviewController.java
@@ -1,13 +1,18 @@
 package com.ssafy.dksl.controller;
 
-import com.ssafy.dksl.model.dto.ReviewDto;
+import com.ssafy.dksl.exception.NonExistReviewException;
+import com.ssafy.dksl.exception.UserNotExistException;
+import com.ssafy.dksl.model.dto.request.ReviewSaveRequestDto;
+import com.ssafy.dksl.model.dto.ReviewSearchDto;
+import com.ssafy.dksl.model.dto.request.ReviewUpdateRequestDto;
+import com.ssafy.dksl.model.dto.response.ReviewUpdateResponseDto;
+import com.ssafy.dksl.model.entity.Review;
 import com.ssafy.dksl.model.service.ReviewService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +24,33 @@ public class ReviewController {
 
     @GetMapping("/{matchId}/{page}")
     public ResponseEntity<?> getMatchReview(@PathVariable String matchId, @PathVariable int page){
-        List<ReviewDto> matchReviews = reviewService.getReviews(matchId, page);
+        List<ReviewSearchDto> matchReviews = reviewService.getReviews(matchId, page);
         return ResponseEntity.ok(matchReviews);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> saveMatchReview(@RequestBody @Valid ReviewSaveRequestDto reviewSaveRequestDto) {
+        try {
+            Review savedReview = reviewService.saveReview(reviewSaveRequestDto);
+
+            if(savedReview==null){
+                return new ResponseEntity<>("리뷰 저장이 정상적으로 이루어지지 않았습니다.", HttpStatus.BAD_REQUEST);
+            }
+
+            return ResponseEntity.ok("리뷰가 정상적으로 등록되었습니다.");
+        } catch (UserNotExistException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/update/")
+    public ResponseEntity<?> updateMatchReview(@RequestBody @Valid ReviewUpdateRequestDto reviewUpdateRequestDto){
+        try {
+            ReviewUpdateResponseDto reviewUpdateResponseDto = reviewService.updateReview(reviewUpdateRequestDto);
+
+            return ResponseEntity.ok(reviewUpdateResponseDto);
+        } catch (NonExistReviewException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
