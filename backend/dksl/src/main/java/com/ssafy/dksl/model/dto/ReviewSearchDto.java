a7aac5a82a6d2c60023215e017545bb1ab4b007b
김태훈 <zaqxsw1397@naver.com>
2023-09-20 17:24:00 +0900

    :recycle: (태훈) ReviewDto를 ReviewSearchDto로 dto의 이름을 변경함

--- backend/dksl/src/main/java/com/ssafy/dksl/model/dto/ReviewSearchDto.java
+++ backend/dksl/src/main/java/com/ssafy/dksl/model/dto/ReviewSearchDto.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 @Builder
-public class ReviewDto {
+public class ReviewSearchDto {
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime createdAt;
