6cb9062057022ad800e4d277e5fc93cfba56b0d7
You Young <dbdud1999@naver.com>
2023-09-22 14:30:56 +0900

    :art: (유영) 토큰 수정

--- backend/dksl/src/main/java/com/ssafy/dksl/model/repository/RefreshTokenRepository.java
+++ backend/dksl/src/main/java/com/ssafy/dksl/model/repository/RefreshTokenRepository.java
@@ -1,6 +1,5 @@
 package com.ssafy.dksl.model.repository;
 
-import com.ssafy.dksl.model.entity.MemberRedis;
 import com.ssafy.dksl.model.entity.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
--- backend/dksl/src/main/java/com/ssafy/dksl/util/JwtUtil.java
+++ backend/dksl/src/main/java/com/ssafy/dksl/util/JwtUtil.java
@@ -19,30 +19,31 @@ import static org.springframework.security.core.authority.AuthorityUtils.createA
 @Component
 public class JwtUtil implements InitializingBean {
 
-    private final long validityInSeconds;  // token 유효 기간
+    private final long accessValidityIn;  // token 유효 기간
+    private final long refreshValidityIn;  // token 유효 기간
     private final String secret;
     private Key key;
 
 
     public JwtUtil(@Value("${jwt.secret}") String secret, @Value("${jwt.token-validity-in-seconds}") long validityInSeconds) {
         this.secret = secret;
-        this.validityInSeconds = validityInSeconds;  // application.properties에 설정
+        this.accessValidityIn = 2 * 60 * 60 * validityInSeconds;  // 2시간
+        this.refreshValidityIn = 14 * 24 * 60 * 60 * validityInSeconds;  // 2주일
     }
 
     // Bean 주입 받은 후 할당
     @Override
     public void afterPropertiesSet() {
-        byte[] keyBytes = Decoders.BASE64.decode(secret);
-        this.key = Keys.hmacShaKeyFor(keyBytes);
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
     // 토큰 정보
-    public String generateToken(String clientId, String role) {
+    public String generateToken(String clientId, String role, boolean isRefresh) {
         Claims claims = Jwts.claims().setSubject(clientId);  // 사용하는 클레임 세트
         claims.put("role", role);  // 임의 역할 부여
-        key = Keys.hmacShaKeyFor(secret.getBytes());
+        // key = Keys.hmacShaKeyFor(secret.getBytes());
 
-        return Jwts.builder().setClaims(claims).setIssuedAt(new Date()).setExpiration(new Date(new Date().getTime() + validityInSeconds)) // 해당 옵션 없으면 expire X
+        return Jwts.builder().setClaims(claims).setIssuedAt(new Date()).setExpiration(new Date(new Date().getTime() + ((isRefresh)? refreshValidityIn : accessValidityIn))) // 해당 옵션 없으면 expire X
                 .signWith(key, SignatureAlgorithm.HS512) // 사용할 암호화 알고리즘과 signature에 들어갈 secret 값
                 .compact();
     }
@@ -55,13 +56,12 @@ public class JwtUtil implements InitializingBean {
     // 토큰 검증
     public boolean validateToken(String token) throws InvalidTokenException {
         try {
-            return Jwts.parser()
-                    .setSigningKey(secret)
-                    .parseClaimsJws(token)
-                    .getBody()
-                    .getExpiration()
-                    .after(new Date());
-        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException e) {
+            throw new InvalidTokenException("구구가가 잘못된 JWT 서명입니다.");
+        }
+        catch (MalformedJwtException e) {
             throw new InvalidTokenException("잘못된 JWT 서명입니다.");
         } catch (ExpiredJwtException e) {
             throw new InvalidTokenException("만료된 JWT 토큰입니다.");
@@ -83,15 +83,15 @@ public class JwtUtil implements InitializingBean {
     // 토큰에 담겨있는 clientId 획득
     public String getClientId(String token) {
         return String.valueOf(Jwts.parserBuilder()
-                .setSigningKey(secret)
+                .setSigningKey(key)
                 .build()
                 .parseClaimsJws(token)
                 .getBody().getSubject());
     }
 
     private String getRole(String accessToken) {
-        return (String) Jwts.parser()
-                .setSigningKey(secret)
+        return Jwts.parser()
+                .setSigningKey(key)
                 .parseClaimsJws(accessToken)
                 .getBody()
                 .get("role", String.class);
