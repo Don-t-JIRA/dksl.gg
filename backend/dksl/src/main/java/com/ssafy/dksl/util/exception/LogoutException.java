0dfbeacdf7949702a5cf92b75aa702dd36876b2b
You Young <dbdud1999@naver.com>
2023-09-22 14:30:04 +0900

    :recycle: (유영) Exception 메시지 리팩토링

--- backend/dksl/src/main/java/com/ssafy/dksl/util/exception/LogoutException.java
+++ backend/dksl/src/main/java/com/ssafy/dksl/util/exception/LogoutException.java
@@ -5,6 +5,6 @@ public class LogoutException extends Exception {
         super("로그아웃을 실패하였습니다.");
     }
     public LogoutException(String message) {
-        super("로그아웃 오류 :: " + message);
+        super("로그아웃 오류 (" + message + ")");
     }
 }
