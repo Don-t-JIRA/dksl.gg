c70019f894f308efe44a4842b4868b2db6d83dd4
김태훈 <zaqxsw1397@naver.com>
2023-09-24 22:53:36 +0900

    :art: (태훈) ReviewSearchResponseDto 수정

--- backend/dksl/src/main/java/com/ssafy/dksl/model/dto/response/ReviewSearchResponseDto.java
+++ backend/dksl/src/main/java/com/ssafy/dksl/model/dto/response/ReviewSearchResponseDto.java
@@ -1,13 +1,10 @@
-package com.ssafy.dksl.model.dto;
+package com.ssafy.dksl.model.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ssafy.dksl.model.entity.User;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.Comment;
 
 import java.time.LocalDateTime;
@@ -16,8 +13,19 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ReviewSearchRequestDto {
+public class ReviewSearchResponseDto {
+    private LocalDateTime createdAt;
     private String clientId;
     private String matchId;
     private String content;
+
+    @Override
+    public String toString() {
+        return "{" +
+                "createdAt=" + createdAt +
+                ", clientId='" + clientId + '\'' +
+                ", matchId='" + matchId + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
