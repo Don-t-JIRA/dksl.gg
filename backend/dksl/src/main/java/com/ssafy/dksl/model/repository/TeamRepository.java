a6ebadee964e9ad9642915eaa1d36e76aa0069aa
유영 <dbdud1999@naver.com>
2023-09-24 21:59:32 +0900

    :art: (유영) 평균 티어 조회 메소드 이동

--- backend/dksl/src/main/java/com/ssafy/dksl/model/repository/TeamRepository.java
+++ backend/dksl/src/main/java/com/ssafy/dksl/model/repository/TeamRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.dksl.model.repository;
 
+import com.ssafy.dksl.model.dto.command.OrderTeamCommand;
 import com.ssafy.dksl.model.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,21 +10,6 @@ import java.util.Map;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
     List<Team> findAllByNameContainingOrDescriptionContaining(String searchName, String searchDescription);
-    /*@Query(value = "SELECT ROW_NUMBER(), AVGT.TierTeam, AVGT.avgTier" + "\n" +
-            "FROM (" +  // 티어 평균 매기기
-                "SELECT TE AS TierTeam, ROUND(AVG(TI.orderNum), 0) AS avgTier FROM Team AS TE" + "\n" +
-                "JOIN MemberTeam AS MT ON MT.team.id = TE.id" + "\n" +
-                "JOIN Member AS ME ON MT.member.id = ME.id" + "\n" +
-                "JOIN Tier AS TI ON ME.tier.id = TI.id" + "\n" +
-                "GROUP BY TE" + "\n" +
-                "ORDER BY AVG(TI.orderNum) ASC" + "\n" +
-            ") AVGT JOIN Tier T ON T.id = AVGT.avgTier"
-    )*/
-    @Query(value = "SELECT ME.teams, ROUND(AVG(TI.orderNum), 0) AS avgTier" + "\n" +
-            "FROM Member AS ME" + "\n" +
-            "JOIN Tier TI ON ME.tier.id = TI.id" + "\n" +
-            "GROUP BY ME.teams")
-    Map<String, Object> findAllOrderByOrderNum();
 
 
 }
