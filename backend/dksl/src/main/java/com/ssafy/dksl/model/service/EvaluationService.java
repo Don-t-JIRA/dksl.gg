dd49fd6c17d83f45f12d9299c802c8da76637a97
김태훈 <zaqxsw1397@naver.com>
2023-09-26 12:24:44 +0900

    :sparkles: (태훈) 유저 평가 Service 단 구현

---
+++ backend/dksl/src/main/java/com/ssafy/dksl/model/service/EvaluationService.java
@@ -0,0 +1,82 @@
+package com.ssafy.dksl.model.service;
+
+import com.ssafy.dksl.exception.EvaluationNotExistException;
+import com.ssafy.dksl.exception.NoEvaluationException;
+import com.ssafy.dksl.exception.NonExistSummonerException;
+import com.ssafy.dksl.model.dto.request.EvaluationCreateRequestDto;
+import com.ssafy.dksl.model.dto.request.EvaluationDeleteRequestDto;
+import com.ssafy.dksl.model.dto.request.EvaluationUpdateRequestDto;
+import com.ssafy.dksl.model.dto.response.EvaluationSearchInfoResponseDto;
+import com.ssafy.dksl.model.dto.response.EvaluationSearchResponseDto;
+import com.ssafy.dksl.model.dto.response.EvaluationUpdateResponseDto;
+import com.ssafy.dksl.model.entity.Evaluation;
+import com.ssafy.dksl.model.entity.User;
+import com.ssafy.dksl.model.repository.EvaluationRepository;
+import com.ssafy.dksl.model.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class EvaluationService {
+    private final EvaluationRepository evaluationRepository;
+    private final UserRepository userRepository;
+
+    public EvaluationSearchInfoResponseDto findEvaluations(String summonerName, int page) throws NoEvaluationException {
+        List<Evaluation> findEvaluations = evaluationRepository.findAllByEvaluateeSummonerName(summonerName, PageRequest.of(page - 1, 10));
+
+        float average = evaluationRepository.findAverageOfEvaluation(summonerName).orElseThrow(() -> new NoEvaluationException("아직 등록된 댓글이 없습니다."));
+
+        List<EvaluationSearchResponseDto> evaluationList = findEvaluations.stream().map(evaluation -> evaluation.to()).collect(Collectors.toList());
+
+        return EvaluationSearchInfoResponseDto.builder()
+                .evaluations(evaluationList)
+                .averageScore(average)
+                .build();
+    }
+
+    @Transactional
+    public boolean createEvaluation(EvaluationCreateRequestDto dto) throws NonExistSummonerException {
+        User evaluatee = userRepository.findByName(dto.getEvaluateeName()).orElseThrow(() -> new NonExistSummonerException("존재하지 않는 소환사에게 댓글을 등록할 수 없습니다."));
+        User evaluator = userRepository.findByName(dto.getEvaluatorName()).orElseThrow(() -> new NonExistSummonerException("존재하지 않는 소환사는 댓글을 등록할 수 없습니다."));
+
+        Evaluation savedEvaluation = Evaluation.builder()
+                .evaluatee(evaluatee)
+                .evaluator(evaluator)
+                .evaluation(dto.getEvaluation())
+                .score(dto.getScore())
+                .build();
+
+        evaluationRepository.save(savedEvaluation);
+
+        return true;
+    }
+
+    @Transactional
+    public EvaluationUpdateResponseDto updateEvaluation(EvaluationUpdateRequestDto dto) throws EvaluationNotExistException {
+        Evaluation updateEvaluation = evaluationRepository.findById(dto.getId()).orElseThrow(() -> new EvaluationNotExistException("존재하지 않는 소환사 평가입니다."));
+
+        updateEvaluation.updateEvaluation(dto);
+
+        return EvaluationUpdateResponseDto.builder()
+                .id(updateEvaluation.getId())
+                .evaluation(updateEvaluation.getEvaluation())
+                .score(updateEvaluation.getScore())
+                .localDateTime(updateEvaluation.getUpdatedAt())
+                .build();
+    }
+
+    @Transactional
+    public boolean deleteEvaluation(EvaluationDeleteRequestDto dto) throws EvaluationNotExistException {
+        Evaluation deleteEvaluation = evaluationRepository.findById(dto.getId()).orElseThrow(() -> new EvaluationNotExistException("존재하지 않는 소환사 평가입니다."));
+
+        deleteEvaluation.deleteEvaluation();
+        return true;
+    }
+}
