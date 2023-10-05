7cde5c2f724b352656547f6bfa30f82081d86677
You Young <dbdud1999@naver.com>
2023-10-05 11:02:09 +0900

    :art: (유영) 예외 처리 변경

--- backend/dksl/src/main/java/com/ssafy/dksl/model/service/LbtiServiceImpl.java
+++ backend/dksl/src/main/java/com/ssafy/dksl/model/service/LbtiServiceImpl.java
@@ -6,6 +6,7 @@ import com.ssafy.dksl.model.dto.response.lbti.LbtiQuestionResponse;
 import com.ssafy.dksl.model.entity.*;
 import com.ssafy.dksl.model.repository.*;
 import com.ssafy.dksl.util.JwtUtil;
+import com.ssafy.dksl.util.exception.MemberNotFoundException;
 import com.ssafy.dksl.util.exception.common.CustomException;
 import com.ssafy.dksl.util.exception.common.InvalidException;
 import lombok.extern.slf4j.Slf4j;
@@ -84,7 +85,7 @@ public class LbtiServiceImpl implements LbtiService {
                 .orElseThrow(() -> new InvalidException("롤BTI"));
 
         if (setLbtiCommand.getAccessToken() == null || setLbtiCommand.getAccessToken().trim().equals("")) return lbti.toLbtiResponse();
-        Member member = memberRepository.findByClientId(jwtUtil.getClientId(setLbtiCommand.getAccessToken())).orElse(null);
+        Member member = memberRepository.findByClientId(jwtUtil.getClientId(jwtUtil.getToken(setLbtiCommand.getAccessToken()))).orElseThrow(MemberNotFoundException::new);
         memberRepository.save(Member.builder()
                 .id(member.getId())
                 .clientId(member.getClientId())
