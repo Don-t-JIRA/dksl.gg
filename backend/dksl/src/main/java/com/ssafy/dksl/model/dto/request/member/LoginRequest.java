6fce1de1cbac05a58f2b4238a1b95f861d152eb6
김태훈 <zaqxsw1397@naver.com>
2023-10-06 00:25:06 +0900

    :recycle: (태훈) RequestDto에 @Builder 어노테이션 샂게하고 @NoArgsConstructor 추가

--- backend/dksl/src/main/java/com/ssafy/dksl/model/dto/request/member/LoginRequest.java
+++ backend/dksl/src/main/java/com/ssafy/dksl/model/dto/request/member/LoginRequest.java
@@ -5,9 +5,10 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Builder
 @Getter
+@NoArgsConstructor
 public class LoginRequest {
     @NotNull
     @Size(max = 20)
