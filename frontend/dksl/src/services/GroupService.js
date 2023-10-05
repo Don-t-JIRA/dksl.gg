2a21036d6d8d0c187e4c874cb839585a07d67dcb
You Young <dbdud1999@naver.com>
2023-10-05 12:01:22 +0900

    :sparkles: (유영) 소속 가입 연동

--- frontend/dksl/src/services/GroupService.js
+++ frontend/dksl/src/services/GroupService.js
@@ -52,6 +52,15 @@ const groupDetail = async (name, hasToken) => {
   }
 };
 
+const joinGroup = async (data) => {
+  try {
+    const response = await auth.post('/team/join', JSON.stringify(data));
+    return response.data;
+  } catch (error) {
+    Swal.fire('Error', error.response.data, 'error');
+  }
+};
+
 const groupLeave = async (name) => {
   try {
     const response = await auth.post('/team/leave', { name });
@@ -70,4 +79,4 @@ const getSummonerGroup = async (name) => {
   }
 }
 
-export { getGroupList, setNewGroup, searchGroup, groupDetail, groupLeave, getSummonerGroup };
+export { getGroupList, setNewGroup, searchGroup, groupDetail, groupLeave, getSummonerGroup, joinGroup };
