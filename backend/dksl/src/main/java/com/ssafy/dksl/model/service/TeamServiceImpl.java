84c8a3f5956ce6fe7438589f15383a22fa08cf3a
You Young <dbdud1999@naver.com>
2023-10-06 11:24:46 +0900

    :bug: (유영) 따옴표 같이 오는 에러 제거

--- backend/dksl/src/main/java/com/ssafy/dksl/model/service/TeamServiceImpl.java
+++ backend/dksl/src/main/java/com/ssafy/dksl/model/service/TeamServiceImpl.java
@@ -138,7 +138,7 @@ public class TeamServiceImpl implements TeamService {
     @Override
     public boolean leaveTeamMember(TeamMemberCommand teamMemberCommand) throws CustomException {
         Member member = memberRepository.findByClientId(jwtUtil.getClientId(teamMemberCommand.getToken())).orElseThrow(MemberNotFoundException::new);
-        Team team = teamRepository.findByNameAndSubmitAtIsNotNull(teamMemberCommand.getTeamName()).orElseThrow(TeamNotFoundException::new);
+        Team team = teamRepository.findByNameAndSubmitAtIsNotNull(teamMemberCommand.getTeamName().replace("\"", "")).orElseThrow(TeamNotFoundException::new);
 
         try {
             memberTeamRepository.delete(MemberTeam.builder().member(member).team(team).build());
